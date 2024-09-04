d82b2354ec11ce5c9111f3dc4ae7c27251e0a44f
Bijay Khadka <bk006822@gmail.com>
2024-09-04 07:16:10 +0000

    added:comments

--- mysite/ads/forms.py
+++ mysite/ads/forms.py
@@ -38,3 +38,7 @@ class CreateForm(forms.ModelForm):
             instance.save()
 
         return instance
+
+class CommentForm(forms.Form):
+    comment = forms.CharField(required=True, max_length=500, min_length=3, strip=True)
+
--- mysite/ads/views.py
+++ mysite/ads/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .owner import OwnerCreateView, OwnerListView, OwnerDetailView, OwnerUpdateView, OwnerDeleteView
-from .models import Ad
-from django.urls import reverse_lazy
+from .models import Ad, Comment
+from django.urls import reverse_lazy, reverse
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
-from ads.forms import CreateForm
+from ads.forms import CreateForm, CommentForm
 
 # Create your views here.
 class AdListView(OwnerListView):
@@ -14,6 +14,14 @@ class AdListView(OwnerListView):
 
 class AdDetailView(OwnerDetailView):
     model = Ad
+    template_name = 'ads/ad_detail.html'
+
+    def get(self, request, pk):
+        ad = get_object_or_404(Ad, id=pk)
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = {'ad': ad, 'comments': comments, 'comment_form': comment_form}
+        return render(request, self.template_name, context)
 
 class AdCreateView(LoginRequiredMixin, View):
     template_name = 'ads/ad_form.html'
@@ -61,6 +69,21 @@ class AdUpdateView(LoginRequiredMixin, View):
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        ad = get_object_or_404(Ad, id=pk)
+        comment = Comment(text= request.POST['comment'], owner=request.user, ad=ad)
+        comment.save()
+        return redirect(reverse('myads:ad_detail', args=[pk]))
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "ads/comment_delete.html"
+
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse('myads:ad_detail', args=[ad.id])
 
 def stream_file(request, pk):
     pic = get_object_or_404(Ad, id=pk)
